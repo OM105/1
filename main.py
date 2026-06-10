da3276623c5396771c75aff75783fe151c858f71
OM105 <your.email@example.com>
2026-06-10 13:29:02 +0530

    US

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ app = FastAPI()
 
 @app.get("/users/me/items/")
 async def read_own_items(
-    current_user: Annotated[User, Depends(get_current_active_user)],
+    current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
 @app.get("/users/me/")
 async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-) -> User:
+    current_user: Annotated[User, Depends(get_current_active_user)]
+) -> UserResponse:
     return current_user
 
 @app.post("/register", response_model = UserResponse)
